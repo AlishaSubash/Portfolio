3eea3eccf9bf904895b6492ba4fda727fb057b74
Alisha Subash <eshasubash163@gmail.com>
2025-07-09 10:14:21 +0530

    host and os removed

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -20,4 +19,4 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ.get("PORT", 10000),debug=True)
+    app.run(debug=True)
